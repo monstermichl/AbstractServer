d94de9b01460e68426a6dd9f12592ee9607ed3bc
monstermichl <monstermichl@users.noreply.github.com>
2022-09-25 19:05:02 +0000

    Generate index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,39 @@
+import {
+    IServerConfig,
+    IServer,
+} from './abstract-server';
+import {
+    RequestMethod,
+    RequestHandlerRequestParams,
+    RequestHandlerResponseParams,
+    RequestHandlerParams,
+    Params,
+    Query,
+    Body,
+    Headers,
+    RequestHandlerInternal,
+    RequestHandler,
+} from './request';
+import {
+    IRoute,
+    ISubRoute,
+} from './route';
 
+export {
+    IServerConfig,
+    IServer,
+    RequestMethod,
+    RequestHandlerRequestParams,
+    RequestHandlerResponseParams,
+    RequestHandlerParams,
+    Params,
+    Query,
+    Body,
+    Headers,
+    RequestHandlerInternal,
+    RequestHandler,
+    IRoute,
+    ISubRoute,
+};
+
+/* Automatically generated on Sun Sep 25 19:05:01 UTC 2022. */
